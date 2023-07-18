b7385d58ba115e7d1debd67b0ce45d6873e63a1b
baifumei <k_katerina_k@icloud.com>
2023-07-18 18:18:28 +0300

    set background (image, url, blank)

--- EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -22,14 +22,17 @@ struct EmojiArtDocumentView: View {
     var documentBody: some View {
         GeometryReader { geometry in
             ZStack {
-                Color.pink
+                Color.white.overlay(
+                    OptionalImage(uiImage: document.backgroundImage)
+                        .position(convertFromEmojiCoordinate((0,0), in: geometry))
+                )
                 ForEach(document.emojis) { emoji in
                     Text(emoji.text)
                         .font(.system(size: fontSize(for: emoji)))
                         .position(position(for: emoji, in: geometry))
                 }
             }
-            .onDrop(of: [.plainText], isTargeted: nil) { providers, location in
+            .onDrop(of: [.plainText,.image,.url], isTargeted: nil) { providers, location in
                 return drop(providers: providers, at: location, in: geometry)
             
             }
@@ -37,15 +40,28 @@ struct EmojiArtDocumentView: View {
     }
     
     private func drop(providers: [NSItemProvider], at location: CGPoint, in geometry: GeometryProxy) -> Bool {
-        return providers.loadObjects(ofType: String.self) { string in
-            if let emoji = string.first, emoji.isEmoji {
-                document.addEmoji(
-                    String(emoji),
-                    at: convertToEmojiCoordinate(location, in: geometry),
-                    size: defaultEmojiFontSize
-                )
+        var found = providers.loadObjects(ofType: URL.self) { url in
+            document.setBackground(EmojiArtModel.Background.url(url.imageURL))
+        }
+        if !found {
+            found = providers.loadObjects(ofType: UIImage.self) { image in
+                if let data = image.jpegData(compressionQuality: 1.0) {
+                    document.setBackground(EmojiArtModel.Background.imageData(data))
+                }
             }
         }
+        if !found {
+            found = providers.loadObjects(ofType: String.self) { string in
+                if let emoji = string.first, emoji.isEmoji {
+                    document.addEmoji(
+                        String(emoji),
+                        at: convertToEmojiCoordinate(location, in: geometry),
+                        size: defaultEmojiFontSize
+                    )
+                }
+            }
+        }
+        return found
     }
     
     private func position(for emoji: EmojiArtModel.Emoji, in geometry: GeometryProxy) -> CGPoint {
